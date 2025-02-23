5dba3f5a0767c751c7d43f624c491865656a5586
dipto230 <diptoroy230305@gmail.com>
2025-02-23 14:13:05 +0600

    animation section done

---
+++ src/styles/Layouts.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+export const MainLayout = styled.div`
+padding: 2 rem;
+height: 100%;
+display: flex;
+gap: 2 rem;
+
+`;
+export const InnerLayout = styled.div`
+  padding: 2 rem 1.5 rem;
+  width: 100%;
+  
+
+`;
